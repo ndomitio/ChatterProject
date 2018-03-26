a85a7611d600a867ac16342c0c38f49b6d1a22b0
NicholasDomitio <nicholas.domitio@gmail.com>
2018-03-26 11:25:39 -0400

    first commit with peter

---
+++ ChatterProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatterProject.Startup))]
+namespace ChatterProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
